6a2ac9e6dfe283aeaebf99d001815bbe8483cbe7
chazz <chazz@dev.com>
2022-09-07 15:10:16 -0400

    set up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth', controllers: {
+    sessions: 'auth/sessions',
+    registrations: 'auth/registrations'
+  }
+
   root 'home#index'
 end
