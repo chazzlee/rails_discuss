702091971b9f5ea2e14788710126ea9ccd4bfc17
chazzlee <chazz@dev.com>
2022-09-22 20:26:03 -0400

    move app header out of main layout into own component

--- app/frontend/components/MainLayout.tsx
+++ app/frontend/components/MainLayout.tsx
@@ -14,6 +14,7 @@ import {
 import { Header } from "@mantine/core";
 
 import { useSharedPageProps } from "../hooks/useSharedPageProps";
+import { AppHeader } from "./AppHeader";
 
 type MainLayoutProps = {
   children: ReactNode;
@@ -22,67 +23,17 @@ type MainLayoutProps = {
 };
 
 // TODO: move to function later
-const _token = document.getElementsByTagName("meta")[2].getAttribute("content");
+// const _token = document.getElementsByTagName("meta")[2].getAttribute("content");
 
 export function MainLayout({
   children,
   newDiscussionLink,
   onShowReplyForm,
 }: MainLayoutProps) {
-  const { currentUser } = useSharedPageProps();
-  const logoutFormRef = useRef<HTMLFormElement>(null);
-
   return (
     <AppShell
       sx={{ backgroundColor: "#F8F9FA" }}
-      header={
-        <Header height={70} p="md" sx={{ backgroundColor: "#F8F9FA" }}>
-          <Container size="xl" pb="sm" mb="sm">
-            <Group position="apart" align="center">
-              <Text size="md" weight={500}>
-                <Link href="/">Rails Discuss</Link>
-              </Text>
-              {/* <Burger opened={false} /> */}
-              {currentUser ? (
-                <Menu width={260} position="bottom-end">
-                  <Menu.Target>
-                    <Button size="sm">{currentUser.data.username}</Button>
-                  </Menu.Target>
-                  <Menu.Dropdown>
-                    <form
-                      action="/auth/sign_out"
-                      method="POST"
-                      id="logout_form"
-                      ref={logoutFormRef}
-                    >
-                      <input type="hidden" name="_method" value="delete" />
-                      <input
-                        type="hidden"
-                        name="authenticity_token"
-                        value={_token as string}
-                      />
-                      <Menu.Item
-                        onClick={() => {
-                          if (logoutFormRef.current) {
-                            logoutFormRef.current.submit();
-                          }
-                        }}
-                      >
-                        Logout
-                      </Menu.Item>
-                    </form>
-                  </Menu.Dropdown>
-                </Menu>
-              ) : (
-                <div>
-                  <a href="/auth/sign_up">Sign Up</a>
-                  <a href="/auth/sign_in">Sign In</a>
-                </div>
-              )}
-            </Group>
-          </Container>
-        </Header>
-      }
+      header={<AppHeader />}
       navbar={
         <Navbar
           p="md"
